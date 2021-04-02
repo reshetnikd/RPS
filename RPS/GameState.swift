131242f5e02ba4de979f3a307c5c30d3000efe78
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-02 16:43:44 +0300

    Extend Sign

---
+++ RPS/GameState.swift
@@ -0,0 +1,12 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Dmitry Reshetnik on 02.04.2021.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
--- Sign.swift
+++ Sign.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+func randomSign() -> Sign {
+    let sign = Int.random(in: 0...2)
+    
+    if sign == 0 {
+        return .rock
+    } else if sign == 1 {
+        return .scissors
+    } else {
+        return .paper
+    }
+}
+
 enum Sign {
     case rock, scissors, paper
     
@@ -18,6 +30,31 @@ enum Sign {
                 return "✌🏻"
             case .paper:
                 return "✋🏻"
+        }
+    }
+    
+    func opponentsTurn(_ sign: Sign) -> GameState? {
+        switch self {
+            case .rock where sign == .rock:
+                return .draw
+            case .rock where sign == .paper:
+                return .lose
+            case .rock where sign == .scissors:
+                return .win
+            case .scissors where sign == .scissors:
+                return .draw
+            case .scissors where sign == .rock:
+                return .lose
+            case .scissors where sign == .paper:
+                return .win
+            case .paper where sign == .paper:
+                return .draw
+            case .paper where sign == .scissors:
+                return .lose
+            case .paper where sign == .rock:
+                return .win
+            default:
+                return nil
         }
     }
 }
