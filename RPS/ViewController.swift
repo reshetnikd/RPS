7b8abfc977ec6ef317da2eb24d88feb8ee9c5f88
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-02 16:11:37 +0300

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Dmitry Reshetnik on 02.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
